b56290cc8c6663d4b126dfb68a8be2a4f7278ebb
Dhruvkumar patel <Iamdhruv1563@gmail.com>
2025-05-04 17:23:18 +0530

    Migrating to the nextfolio template
    - Changed the codebase with the Nextfolio boilerplate.

---
+++ app/config.ts
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "https://stack-dhruv.vercel.app/",
+  title: "Dhruvkumar Patel",
+  name: "stack-dhruv",
+  ogImage: "/opengraph-image.png",
+  description:
+    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+};
+
+export const socialLinks = {
+twitter: "https://x.com/dhruv1563",
+  github: "https://github.com/stack-dhruv/",
+  instagram: "https://www.instagram.com/dhruv_1563_",
+  linkedin: "https://www.linkedin.com/in/stack-dhruv",
+  email: "mailto:iamdhruv1563@gmail.com",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./public/**/*.svg"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      typography: {
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+          },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
