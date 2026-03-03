25b0ae4dee246f6ff05e085ecaed17730ac94dc9
ridh21 <ridhampatel2k4@gmail.com>
2026-03-03 09:13:32 +0530

    refactor: Improve footer semantics, encapsulate MongoDB client initialization, and update development dependencies.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -25,7 +25,7 @@ function SocialLink({ href, icon: Icon }) {
 function SocialLinks() {
   return (
     <div className="flex text-lg mt-2 gap-3.5 float-right transition-opacity duration-300 hover:opacity-90 text-[var(--color-contrast-low)]">
-      <SocialLink href={socialLinks.github}  icon={IconGitHub} />
+      <SocialLink href={socialLinks.github} icon={IconGitHub} />
       <SocialLink href={socialLinks.linkedin} icon={IconLinkedIn} />
       <SocialLink href={socialLinks.twitter} icon={IconTwitterX} />
       <SocialLink href={socialLinks.instagram} icon={IconInstagram} />
@@ -40,28 +40,10 @@ function SocialLinks() {
 
 export default function Footer() {
   return (
-    <small className="block lg:mt-12 mt-8 text-[var(--color-contrast-medium)]">
-      {/* <a
-        className="btn btn-ghost text-xs gap-1.5 no-underline px-2"
-        href="/resume.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        View Resume
-      </a>
-      <style jsx>{`
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
-      <SocialLinks /> */}
-      <div className=" border-b border-[var(--color-border)]"></div>
-        
+    <footer className="block lg:mt-12 mt-8 text-[var(--color-contrast-medium)]">
+      <div className="border-b border-[var(--color-border)]" />
       <p className="mt-5 text-xs flex justify-center gap-1 text-[var(--color-contrast-low)]">
-        Portfolio inspired by{"  "}
+        Portfolio inspired by{" "}
         <a
           href="https://stack-dhruv.vercel.app"
           target="_blank"
@@ -71,6 +53,6 @@ export default function Footer() {
           Dhruvkumar Patel
         </a>
       </p>
-    </small>
+    </footer>
   );
 }
